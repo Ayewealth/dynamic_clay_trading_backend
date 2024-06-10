f84d673d7e0d7aa38031f6220b6cd4f24e74febe
Anwasi Ayebaemi <ayewealth11@gmail.com>
2024-06-10 22:05:39 +0100

    Settings

--- dynamic_clay_trading_backend/settings.py
+++ dynamic_clay_trading_backend/settings.py
@@ -19,7 +19,7 @@ import os
 env = Env()
 Env.read_env()
 ENVIRONMENT = env('ENVIRONMENT', default='production')
-POSTGRES_LOCALLY = False
+POSTGRES_LOCALLY = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
